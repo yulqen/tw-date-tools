dd13cc73c244400f8924dd497097559afac5d735
Matthew Lemon <lemon@matthewlemon.com>
2017-12-06 22:10:07 +0000

    added comment

--- tw_holidays/tests/test_geturl.py
+++ tw_holidays/tests/test_geturl.py
@@ -13,6 +13,7 @@ data = """{"locale":"en-GB","region":"","date":"2017-01-01","description":"New Y
 {"locale":"en-GB","region":"","date":"2017-12-26","description":"Boxing Day","type":"NF","notes":""}"""
 
 
+# don't need the decorator here but leaving it for educational purposes for the moment
 @patch('tw_holidays.http.APIResource')
 def test_get_data(mock_class):
     ar = APIResource('https://holidata.net/en-GB/2017.json')
