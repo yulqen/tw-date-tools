57887c2cadeb5e7bcf2b8f4f479afbf09d12bfda
Matthew Lemon <lemon@matthewlemon.com>
2017-12-16 21:12:11 +0000

    parsed holidays from rc file into object - test passing

---
+++ tdt/parser.py
@@ -0,0 +1,50 @@
+import re
+import datetime
+
+
+class RCParser:
+
+    date_line = re.compile(r'holiday\.(\w{2})-([A-Z]{2})(\d{1,2})\.date=(\d{8})')
+    name_line = re.compile(r'holiday\.(\w{2})-([A-Z]{2})(\d{1,2})\.name=(.+$)')
+
+    def __init__(self, f: str) -> None:
+        self.rc_file = f
+        self.language = ""
+        self.country = ""
+        self.index = None
+        self.date = None
+        self.holidays: dict = {}
+        self._parse()
+
+    def _parse(self):
+
+        main_data_matched = False
+
+        with open(self.rc_file, 'r') as f:
+            lines = f.readlines()
+            for x in lines:
+                n_match = re.match(RCParser.name_line, x.strip())
+                if n_match:
+                    if not main_data_matched:
+                        self.language = n_match.group(1)
+                        self.country = n_match.group(2)
+                        self.index = int(n_match.group(3))
+                        self.holidays[n_match.group(4)] = None
+                        hol = n_match.group(4)
+                        main_data_matched = True
+                        continue
+                    else:
+                        self.holidays[n_match.group(4)] = None
+                        hol = n_match.group(4)
+                        continue
+                d_match = re.match(RCParser.date_line, x.strip())
+                if d_match:
+                    if d_match.group(4)[4:6][0] == '0':
+                        _month = d_match.group(4)[5]
+                    else:
+                        _month = d_match.group(4)[4:6]
+                    self.holidays[hol] = datetime.date(
+                        int(d_match.group(4)[0:4]),
+                        int(_month),
+                        int(d_match.group(4)[6:8])
+                    )
--- tdt/tests/test_parser.py
+++ tdt/tests/test_parser.py
@@ -1,4 +1,24 @@
+import datetime
+from ..parser import RCParser
+
 def test_file_presence(holiday_rc_file):
     with open(holiday_rc_file, 'r') as f:
         line = f.readline()
         assert line[0] == '#'
+
+
+def test_rc_language(holiday_rc_file):
+    rc = RCParser(holiday_rc_file)
+    assert rc.language == 'en'
+    assert rc.country == 'GB'
+
+
+def test_rc_holiday_dict(holiday_rc_file):
+    rc = RCParser(holiday_rc_file)
+    import pdb; pdb.set_trace()  # XXX BREAKPOINT
+    assert "New Year's Day" in rc.holidays.keys()
+    assert "Good Friday" in rc.holidays.keys()
+
+#def test_contains_date_line(holiday_rc_file):
+#    rc_parser = rc_parser(holiday_rc_file)
+#    assert rc_parser.language == 'en'
