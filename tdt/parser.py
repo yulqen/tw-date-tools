98f8fb4e5a5c48a6e10db2aa6b1c29744e8dbb57
Matthew Lemon <lemon@matthewlemon.com>
2017-12-16 22:41:07 +0000

    added mocked test

--- tdt/parser.py
+++ tdt/parser.py
@@ -1,11 +1,26 @@
+import os
 import re
 import datetime
+
+from typing import List
+
+rc_file_format = re.compile(r'holidays\.(\w{2})-([A-Z]{2})\.rc')
+date_line = re.compile(r'holiday\.(\w{2})-([A-Z]{2})(\d{1,2})\.date=(\d{8})')
+name_line = re.compile(r'holiday\.(\w{2})-([A-Z]{2})(\d{1,2})\.name=(.+$)')
+
+rc_locs = [
+    '/usr/share/taskwarrior',
+    # other platforms
+]
+
+
+def rc_paths() -> List[str]:
+    return os.listdir(rc_locs[0])
+
 
 
 class RCParser:
 
-    date_line = re.compile(r'holiday\.(\w{2})-([A-Z]{2})(\d{1,2})\.date=(\d{8})')
-    name_line = re.compile(r'holiday\.(\w{2})-([A-Z]{2})(\d{1,2})\.name=(.+$)')
 
     def __init__(self, f: str) -> None:
         self.rc_file = f
@@ -21,7 +36,7 @@ class RCParser:
         with open(self.rc_file, 'r') as f:
             lines = f.readlines()
             for x in lines:
-                n_match = re.match(RCParser.name_line, x.strip())
+                n_match = re.match(name_line, x.strip())
                 if n_match:
                     if not main_data_matched:
                         self.language = n_match.group(1)
@@ -32,7 +47,7 @@ class RCParser:
                     else:
                         hol = n_match.group(4)
                         continue
-                d_match = re.match(RCParser.date_line, x.strip())
+                d_match = re.match(date_line, x.strip())
                 if d_match:
                     if d_match.group(4)[4:6][0] == '0':
                         _month = d_match.group(4)[5]
--- tdt/tests/test_parser.py
+++ tdt/tests/test_parser.py
@@ -1,5 +1,6 @@
-import datetime
-from ..parser import RCParser
+import os
+from ..parser import RCParser, rc_paths
+from unittest.mock import patch
 
 
 def test_file_presence(holiday_rc_file):
@@ -26,3 +27,16 @@ def test_other_file_metadata(holiday_rc_file):
     rc = RCParser(holiday_rc_file)
     assert rc.language == "en"
     assert rc.country == "GB"
+
+
+def test_rc_files_in_system():
+    with patch('os.listdir', return_value=[
+        'holidays.be-BY.rc',
+        'holidays.cs-CZ.rc',
+        'holidays.da-DK.rc',
+        'holidays.de-AT.rc',
+        'holidays.de-BE.rc',
+        'holidays.de-CH.rc'
+    ]):
+        ps = rc_paths()
+        assert ps[0] == 'holidays.be-BY.rc'
